111abf80cd4409d0ef7b8d117483c9dc1d58d6e9
DeadKper <missaga@proton.me>
2024-06-02 14:18:09 -0700

    better lualine highlights

--- lua/lualine/themes/krbon.lua
+++ lua/lualine/themes/krbon.lua
@@ -1,30 +1,39 @@
 local c = require("krbon.colors")
 local cfg = vim.g.krbon
 
+local bg = {
+	a = c.bg0,
+	b = c.bg2,
+	c = cfg.transparent.statusline and c.none or c.bg1,
+}
+
 local krbon = {
 	inactive = {
-		a = { fg = c.gray, bg = c.bg0, gui = "bold" },
-		b = { fg = c.gray, bg = c.bg0 },
-		c = { fg = c.gray, bg = cfg.transparent.statusline and c.none or c.bg1 },
+		a = { fg = c.fg1, bg = bg.a, gui = "bold" },
+		b = { fg = c.fg1, bg = bg.b },
+		c = { fg = c.fg1, bg = bg.c },
 	},
 	normal = {
-		a = { fg = c.bg0, bg = c.lavender, gui = "bold" },
-		c = { fg = c.fg0, bg = cfg.transparent.statusline and c.none or c.bg1 },
+		a = { bg = c.lavender },
+		c = { fg = c.fg0, bg = bg.c },
 	},
-	visual = { a = { fg = c.bg0, bg = c.blue, gui = "bold" } },
-	replace = { a = { fg = c.bg0, bg = c.magenta, gui = "bold" } },
-	insert = { a = { fg = c.bg0, bg = c.pink, gui = "bold" } },
-	command = { a = { fg = c.bg0, bg = c.green, gui = "bold" } },
-	terminal = { a = { fg = c.bg0, bg = c.fg0, gui = "bold" } },
+	visual = { a = { bg = c.blue } },
+	replace = { a = { bg = c.magenta } },
+	insert = { a = { bg = c.pink } },
+	command = { a = { bg = c.green } },
+	terminal = { a = { bg = c.fg0 } },
 }
 
 for key, value in pairs(krbon) do
 	if key ~= "inactive" then
 		value.b = {
 			fg = value.a.bg,
-			bg = value.a.fg,
+			bg = bg.b,
 			gui = "bold",
 		}
+
+		value.a.fg = bg.b
+		value.a.gui = "bold"
 	end
 end
 
